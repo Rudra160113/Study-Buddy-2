df08cbc02dc6257ec1f645f7c89adabcd1962abd
Rajani Pandey <rajanipandeyrnk@gmail.com>
2025-06-26 10:59:24 +0000

    Create a firebase backend or file so that it can be easily accessed thro

---
+++ src/lib/firebase.ts
@@ -0,0 +1,23 @@
+// Import the functions you need from the SDKs you need
+import { getApp, getApps, initializeApp, type FirebaseOptions } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig: FirebaseOptions = {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+};
+
+// Initialize Firebase
+// We add a check to prevent re-initialization on hot-reloads in development
+const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
+
+// Export the necessary Firebase services for use throughout the app
+export const db = getFirestore(app);
+export const auth = getAuth(app);
